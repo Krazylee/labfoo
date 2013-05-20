64d40b5f9fcce50652ef231abec9a444772b31bb
krazy <lixiangstar@gmail.com>
2013-05-20 16:15:55 +0800

    add drag-drop

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,14 @@
 'use strict';
 
+
 angular.module('modeApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.menus = [
-      '前面板',
-      '后面板'
-    ];
-	$(".dial").knob();
-  });
+	$scope.menus = [
+		'前面板',
+		'后面板'
+	];
+	$scope.initKnob = function() {
+		alert("a")
+		$(".dial").knob();
+	}
+});
